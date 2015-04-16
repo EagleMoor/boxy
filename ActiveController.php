8386906c20139e57686035d2fccb479f9fafe21b
Алексей Волегов <eaglemoor@webspirit.pro>
2015-04-16 16:54:19 +0300

    up activeController & create action

---
+++ ActiveController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eaglemoor
+ * Date: 16.04.15
+ * Time: 16:49
+ */
+
+namespace yii\boxy;
+
+class ActiveController extends \yii\rest\ActiveController {
+
+    use ControllerTrait;
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        if (isset($actions['create'])) {
+            $actions['create']['class'] = 'yii\boxy\CreateAction';
+        }
+        return $actions;
+    }
+
+}
