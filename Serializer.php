1795c0b50a79782737b59b97baa847c4bc276df9
Алексей Волегов <eaglemoor@webspirit.pro>
2015-06-07 23:51:09 +0300

    back

--- Serializer.php
+++ Serializer.php
@@ -12,21 +12,45 @@ namespace yii\boxy;
 class Serializer extends \yii\rest\Serializer {
     public $afterSerializeModels;
 
-    /**
-     * Serializes a set of models.
-     * @param array $models
-     * @return array the array representation of the models
-     */
-    protected function serializeModels(array $models)
-    {
+//    /**
+//     * Serializes a set of models.
+//     * @param array $models
+//     * @return array the array representation of the models
+//     */
+//    protected function serializeModels(array $models)
+//    {
+////        var_dump($models); die;
+//        $result = parent::serializeModels($models);
+////        var_dump($result); die;
+//
+////        if ($this->afterSerializeModels) {
+////            return call_user_func($this->afterSerializeModels, $result);
+////        }
+//
+//        return $models;
+//    }
+
+//    /**
+//     * Serializes a set of models.
+//     * @param array $models
+//     * @return array the array representation of the models
+//     */
+//    protected function serializeModels(array $models)
+//    {
+//        list ($fields, $expand) = $this->getRequestedFields();
+//        foreach ($models as $i => $model) {
+//            if ($model instanceof Arrayable) {
+//                $models[$i] = $model->toArray($fields, $expand);
+//            } elseif (is_array($model)) {
+//                $models[$i] = ArrayHelper::toArray($model);
+//            }
+//        }
+//
+////        if ($this->afterSerializeModels) {
+////            return call_user_func($this->afterSerializeModels, $models);
+////        }
+//
 //        var_dump($models); die;
-        $result = parent::serializeModels($models);
-//        var_dump($result); die;
-
-        if ($this->afterSerializeModels) {
-            return call_user_func($this->afterSerializeModels, $result);
-        }
-
-        return $models;
-    }
+//        return $models;
+//    }
 }
