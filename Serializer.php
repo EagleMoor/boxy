d05793c2f37294d9ee2b937b576dbfaccaf6e5b4
Алексей Волегов <eaglemoor@webspirit.pro>
2015-06-07 23:14:22 +0300

    Data mapping

--- Serializer.php
+++ Serializer.php
@@ -10,4 +10,23 @@ namespace yii\boxy;
 
 
 class Serializer extends \yii\rest\Serializer {
+    public $afterSerializeModels;
+
+    /**
+     * Serializes a set of models.
+     * @param array $models
+     * @return array the array representation of the models
+     */
+    protected function serializeModels(array $models)
+    {
+//        var_dump($models); die;
+        $result = parent::serializeModels($models);
+//        var_dump($result); die;
+
+        if ($this->afterSerializeModels) {
+            return call_user_func($this->afterSerializeModels, $result);
+        }
+
+        return $models;
+    }
 }
