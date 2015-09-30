01875358f18be4cbd1fac16d81ebfcfef3de1d5f
Алексей Волегов <eaglemoor@webspirit.pro>
2015-09-30 12:32:01 +0300

    Google Maps center

--- maps/google/StaticMaps.php
+++ maps/google/StaticMaps.php
@@ -7,7 +7,10 @@
  */
 namespace yii\maps\google;
 
-class StaticMaps extends \yii\base\Object {
+use yii\maps\PointInterface;
+
+class StaticMaps extends \yii\base\Object
+{
     public $url = 'https://maps.googleapis.com/maps/api/staticmap?key={key}&language={language}';
 
     public $width = 400;
@@ -15,27 +18,36 @@ class StaticMaps extends \yii\base\Object {
 
     public $zoom = 14;
 
+    /** @var PointInterface */
+    protected $_center;
+
+    /** @var array Path */
     protected $_paths = [];
+
+    /** @var array Marker */
     protected $_markers = [];
 
     public $key;
     public $language;
 
-    public function addMarket(\yii\maps\Marker $marker) {
+    public function addMarket(\yii\maps\Marker $marker)
+    {
         $this->_markers[] = $marker;
     }
 
     /**
      * @return array|\yii\maps\Marker[]
      */
-    public function getMarkers() {
+    public function getMarkers()
+    {
         return $this->_markers;
     }
 
     /**
      * @param \yii\maps\Marker[] $markers
      */
-    public function setMarkers(array $markers) {
+    public function setMarkers(array $markers)
+    {
         $this->_markers = [];
         foreach ($markers as $marker) {
             $this->addMarket($marker);
@@ -45,28 +57,48 @@ class StaticMaps extends \yii\base\Object {
     /**
      * @param \yii\maps\Path $path
      */
-    public function addPath(\yii\maps\Path $path) {
+    public function addPath(\yii\maps\Path $path)
+    {
         $this->_paths[] = $path;
     }
 
     /**
      * @return array|\yii\maps\Path[]
      */
-    public function getPaths() {
+    public function getPaths()
+    {
         return $this->_paths;
     }
 
     /**
      * @param array|\yii\maps\Path[] $paths
      */
-    public function setPaths(array $paths) {
+    public function setPaths(array $paths)
+    {
         $this->_paths = [];
         foreach ($paths as $path) {
             $this->addPath($path);
         }
     }
 
-    public function buildUrl() {
+    /**
+     * @param PointInterface $point
+     */
+    public function setCenter(PointInterface $point)
+    {
+        $this->_center = $point;
+    }
+
+    /**
+     * @return PointInterface
+     */
+    public function getCenter()
+    {
+        return $this->_center;
+    }
+
+    public function buildUrl()
+    {
         $url = str_replace(['{key}', '{language}'], [$this->key, $this->language], $this->url);
 
         $params = [];
@@ -75,14 +107,18 @@ class StaticMaps extends \yii\base\Object {
 
         if ($this->getMarkers()) {
             foreach ($this->getMarkers() as $marker) {
-                $params[] = (string) $marker;
+                $params[] = (string)$marker;
             }
         }
 
         if ($this->getPaths()) {
             foreach ($this->getPaths() as $path) {
-                $params[] = (string) $path;
+                $params[] = (string)$path;
             }
+        }
+
+        if ($this->getCenter()) {
+            $params[] = 'center=' . $this->getCenter()->lat() . ',' . $this->getCenter()->lon();
         }
 
         if ($this->zoom)
@@ -91,7 +127,8 @@ class StaticMaps extends \yii\base\Object {
         return $url . '&' . implode('&', $params);
     }
 
-    public function __toString() {
+    public function __toString()
+    {
         return $this->buildUrl();
     }
 }
