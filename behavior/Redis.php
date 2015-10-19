de1d5e6ddcfec43785ca2fb9538efa8661de5452
Алексей Волегов <eaglemoor@webspirit.pro>
2015-10-19 12:57:27 +0300

    add Redis behavior for parsing hash

---
+++ behavior/Redis.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eaglemoor
+ * Date: 19.10.15
+ * Time: 11:07
+ */
+
+namespace yii\boxy\behavior;
+
+
+class Redis extends \yii\base\Behavior
+{
+    /**
+     * Получение hash данных в виде массива или объекта
+     *
+     * @param string $key
+     * @param string|array|callable $class  the object type.
+     * @return array|object|false
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function getHash($key, $class = null)
+    {
+        try {
+            $data = $this->owner->hgetall($key);
+        } catch (\Exception $e) {
+            return false;
+        }
+        $row = [];
+        $c = count($data);
+        for ($i = 0; $i < $c;) {
+            $row[$data[$i++]] = $data[$i++];
+        }
+
+        if (null === $class) return $row;
+        else {
+            $object = \Yii::createObject($class);
+            foreach ($row as $key => $value) {
+                if ($object->hasProperty($key)) $object->$key = $value;
+            }
+
+            return $object;
+        }
+    }
+}
